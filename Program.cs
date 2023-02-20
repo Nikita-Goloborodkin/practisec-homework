63eaf71d21cacd62c8640c0226638d46c2d8d229
Nikita-Goloborodkin <124371973+Nikita-Goloborodkin@users.noreply.github.com>
2023-02-20 19:14:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿void FillNumber(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length){
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintNumber(int[] call)
+{
+    int count = call.Length;
+    int position = 0;
+    while(position<count){
+        Console.WriteLine(call[position]);
+        position++;
+    }
+}
+int Indexfind (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return  position;
+}
+int[] some = new int[10];
+FillNumber(some);
+some[4] = 4;
+some[5] = 4;
+PrintNumber(some);
+
+
+Console.WriteLine();
+int rez = Indexfind(some, 434);
+Console.WriteLine(rez);
